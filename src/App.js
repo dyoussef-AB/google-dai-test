0052828bcff02c0a1292e06b32623e72d45eef91
dyoussef <86694002+dyoussef-AB@users.noreply.github.com>
2023-05-30 11:07:59 +1000

    d

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -44,18 +43,6 @@ function App() {
           </div>
           <div id="companion"></div>
         </div>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
     </div>
   );
 }
